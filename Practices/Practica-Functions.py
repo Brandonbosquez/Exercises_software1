13227668adeb04dcce2108bd5ccaf3295ba2e172
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-08 15:25:36 +0300

    guardar

--- Practices/Practica-Functions.py
+++ Practices/Practica-Functions.py
@@ -1,9 +1,22 @@
 #Define / declare the function
 def hello() :
     print("Hello World!")
+    for i in range(5) :
+        print("----------------")
     print("Goodbye world!")
     return
 
 hello()
 print("mierda de relleno")
 hello()
+
+def minus(argument):
+    if argument > 0 :
+        return  - argument
+    else :
+        return argument
+
+number = int(input("Give a number: "))
+while number!= 0
+    print(minus(number))
+    number = int(input("Give a number: "))
