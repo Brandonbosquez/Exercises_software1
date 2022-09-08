7ed97efff5c84002d99593d97c531f74f03e8c2d
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-08 10:03:46 +0300

    ex04

--- Excercise04.py
+++ Excercise04.py
@@ -74,13 +74,17 @@ points = []
 
 n = 0
 run = int(input("Amount of points to be generated: "))
+ins = 0
 while n != run :
     x = random.randint(-1, 1)
     y = random.randint(-1, 1)
-    points.append(x)
-    points.append(y)
+    points.insert(x)
+    points.insert(y)
     n = n + 1
+    if point[0] ** 2 + points[1] ** 2 < 1
+        ins = ins + 1
 print(points)
+
 
 
 
