0dbc2b837d9dc8759180842c23cb235eb79d46d0
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-08 10:39:52 +0300

    ex04

--- Practices/Hiekkalaatikko.py
+++ Practices/Hiekkalaatikko.py
@@ -1,16 +1,14 @@
 #Practicas de correr
-points = []
 import random
 n = 0
-run = int(input("Amount of points to be generated: "))
+run = float(input("Amount of points to be generated: "))
 ins = 0
 while n != run :
-    x = random.randint(-1, 1)
-    y = random.randint(-1, 1)
-    points.insert(0, x)
-    points.insert(1,y)
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
     n = n + 1
-    if points[0] ** 2 + points[1] ** 2 < 1 :
+    if ((points[0] ** 2)+ (points[1] ** 2)) < 1 :
         ins = ins + 1
 print(points)
 pi = float
