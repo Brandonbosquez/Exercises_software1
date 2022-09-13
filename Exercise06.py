7b589f07338ec56908a9a8f6c4e8d4630603281e
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-13 17:14:56 +0300

    ex06 progress

--- Exercise06.py
+++ Exercise06.py
@@ -2,11 +2,35 @@
 import random
 
 def roll():
-    dice = random.randint(1,6)
-    while dice != 6 :
-        print(dice)
-        dice = random.randint(1, 6)
-        print(dice)
-    else:
-        print("The dice roll is 6, PROGRAM ENDED")
+    return random.randint(1,6)
+
 roll()
+while roll() != 6:
+    print(roll())
+    roll()
+    print(roll())
+else:
+    print("The dice roll is 6, PROGRAM ENDED")
+
+#2
+
+print("I will give the dice as many sides as you want, and roll it until it hits the maximum value")
+def roll2(x):
+    return random.randint(1,x)
+x = int(input("How many sides should the dice have? "))
+roll2(x)
+while roll2(x) != x :
+    print(roll2(x))
+else:
+    print("The dice has reached ", x, ", PROGRAM ENDED")
+
+#3
+print("US Gallon to Liter converter. To end the program input a negative value")
+def gallit(g):
+    return g * 4.546
+g = int(input("How many US Gallons do you want to convert to Liters?: "))
+while g > 0 :
+    print(g, " Gallons is: ", gallit(g), " Liters")
+    g = int(input("How many US Gallons do you want to convert to Liters?: "))
+else:
+    print("Negative value entered. PROGRAM ENDED")
