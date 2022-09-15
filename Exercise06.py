9aebf2fa7cf84dba33d52f9a69658d720181404d
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-15 13:03:21 +0300

    Ex06 TERMINADO

--- Exercise06.py
+++ Exercise06.py
@@ -40,11 +40,38 @@ else:
 def adder(list):
     add = 0
     for x in range(len(list)):
-        sum += list[x]
-    return sum
+        add += list[x]
+    return add
 numlist = [850, 980, 130 ]
 print(adder(numlist))
+print("ENDED PROBLEM 4")
 
 
 #5
+def even(list):
+    for x in range(len(list)) :
+        if x % 2 != 0 :
+            list.remove(x)
+    return list
+numero = [1,2,3,4,5,6,7,8,9,10]
+print(even(numero))
 
+#6 EJERCICIO 6
+
+import math
+def pizza(dia, cost):
+    area = ( ( dia / 2 ) ** 2 * math.pi ) / 100
+    value = cost / area
+    return value
+print("I will tell you, between two pizzas, which one gives you the best value for your buck! ;)")
+dia1 = int(input("Please enter the diameter in cm of pizza #1: "))
+cost1 = int(input("Please enter the price in euros of pizza #1: "))
+print("Now give me the data of pizza #2")
+dia2 = int(input("Please enter the diameter in cm of pizza #2: "))
+cost2 = int(input("Please enter the price in euros of pizza #2: "))
+valor1= pizza(dia1,cost1)
+valor2 = pizza(dia2,cost2)
+if valor1 < valor2 :
+    print("Pizza #1 has a lower price per square meter. BUY PIZZA #1 !!!")
+else:
+    print("Pizza #2 has a lower price per square meter. BUY PIZZA #2 !!!")
--- Practices/Hiekkalaatikko.py
+++ Practices/Hiekkalaatikko.py
@@ -1,11 +1,19 @@
 #Practicas de correr
-
-def adder(nums):
-  print("here is the sum of the values")
-  for x in nums :
-      x + x
-      return x
-number = [1,2,3,4]
-adder(number)
-
+import math
+def pizza(dia, cost):
+    area = ( ( dia / 2 ) ** 2 * math.pi ) / 100
+    value = cost / area
+    return value
+print("I will tell you, between two pizzas, which one gives you the best value for your buck! ;)")
+dia1 = int(input("Please enter the diameter in cm of pizza #1: "))
+cost1 = int(input("Please enter the price in euros of pizza #1: "))
+print("Now give me the data of pizza #2")
+dia2 = int(input("Please enter the diameter in cm of pizza #2: "))
+cost2 = int(input("Please enter the price in euros of pizza #2: "))
+valor1= pizza(dia1,cost1)
+valor2 = pizza(dia2,cost2)
+if valor1 < valor2 :
+    print("Pizza #1 has a lower price per square meter. BUY PIZZA #1 !!!")
+else:
+    print("Pizza #2 has a lower price per square meter. BUY PIZZA #2 !!!")
 
