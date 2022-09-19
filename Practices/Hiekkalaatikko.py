b1cb1dbfa8e214fa713df867e899639379892306
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-19 14:03:02 +0300

    Ex07 progreso

--- Practices/Hiekkalaatikko.py
+++ Practices/Hiekkalaatikko.py
@@ -1,18 +1,34 @@
 #Practicas de correr
 
-#2
+#3 Problema 3 ----------------
+print("Welcome to the Airport database")
+data = {"EFHK" : "Helsinki-Vantaa Airport",
+        "MPTO" : "Tocumen International airport",
+        "KMIA" : "Miami International airport"}
+option = int(input("What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press ENTER "))
 
-direc = set()
-print("Give me names and I will tell you if I have them or not")
-print("To end the program enter empty string")
-name = input("Enter a name: ")
-while name != "":
-
-    if name in direc :
-        print("EXISTING NAME")
-    else:
-        print("NEW NAME")
-    direc.add(name)
-    name = input("Enter a name: ")
-print(direc)
-
+while option != "":
+    while option == 1 :
+        print("Selected FETCH")
+        code = input("Please enter the ICAO code of the airport: ")
+        while option != "" :
+            if code in data :
+                print(f"The code {code} corresponds to {data[code]}")
+                option = int(input("What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press ENTER "))
+                code = input("Please enter the ICAO code of the airport: ")
+                if option = "":
+                    print("PROGRAM ENDED")
+                    break
+            else :
+                print("Code was not found in the database")
+                option = int(input("What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press ENTER "))
+    while option == 2 :
+        print("Selected ADD")
+        code = input("Add the new code: ")
+        airport = input("Add the name of the airport: ")
+        data[code] = airport
+        print (f"The code {code} corresponds to {airport}")
+        option = int(input("What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press ENTER "))
+        if option = "":
+            print("PROGRAM ENDED")
+            break
