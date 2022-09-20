db64e9ab23305ca9068531d4c9f7699d45acc87e
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-20 13:50:15 +0300

    Ex08 progreso

--- Practices/Arenero1.py
+++ Practices/Arenero1.py
@@ -1,33 +1,23 @@
 #lugar de practica de programación
 
-print("Welcome to the Airport database")
-data = {"EFHK" : "Helsinki-Vantaa Airport",
-        "MPTO" : "Tocumen International airport",
-        "KMIA" : "Miami International airport"}
-option = int(input("What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press 3 "))
-while option != 3 :
-    if option == 1 :
-        code = input("Enter the ICAO code of the airport: ")
-        if code in data:
-            print(f"The code {code} corresponds to {data[code]}")
-            option = int(input(
-                "What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press 3 "))
-            if option == "":
-                print("PROGRAM ENDED")
-                break
-        else:
-            print("Code was not found in the database")
-            option = int(input(
-                "What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press 3 "))
-    if option == 2 :
-        print("Selected ADD")
-        code = input("Add the new code: ")
-        airport = input("Add the name of the airport: ")
-        data[code] = airport
-        print(f"The code {code} corresponds to {airport}")
-        option = input(
-            "What do you want to do?" "\n""To fetch data type 1""\n" "To add data type 2""\n""To quit press ENTER ")
-    else:
-        print("PROGRAM ENDED")
-        break
-print("Thank you for using the database :)")
+import mysql.connector
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port= 3306,
+    database ='flight_game',
+    user = 'root',
+    password = 'MariaDB123',
+    autocommit = True
+    )
+def codex(code):
+    sql = "SELECT airport.name, municipality, FROM airport WHERE airport.ident = " + code
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        for row in result :
+            print(f"hola")
+    return
+code = input("Enter ICAO code: ")
+codex(code)
--- Practices/Hiekkalaatikko.py
+++ Practices/Hiekkalaatikko.py
@@ -1,5 +1,6 @@
 #Practicas de correr
 
+
 import mysql.connector
 connection = mysql.connector.connect(
     host='127.0.0.1',
@@ -9,3 +10,11 @@ connection = mysql.connector.connect(
     password = 'MariaDB123',
     autocommit = True
     )
+
+
+
+code = input("Enter ICAO code: ")
+SELECT airport.name, airport.municipality, FROM airport WHERE airport.ident = "00AA" ;
+cursor = connection.cursor()
+cursor.execute()
+result = cursor.fetchall()
